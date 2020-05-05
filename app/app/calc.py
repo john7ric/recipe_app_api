838301c1dec7b1abb5c38b8dec62c6bc5396e4fc
A-7222 <richard.j.ext@flydubai.com>
2020-05-05 21:59:26 +0530

    tdd sampled

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,3 +1,8 @@
-def add_numbers(x, y):
+def add(x, y):
     """ method for adding two strings together """
     return x+y
+
+
+def substract(x, y):
+    """method to subtract two numbers and return """
+    return x-y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
-from app.calc import add_numbers
+from app.calc import add, substract
 
 
 class TestCalc(TestCase):
     """ test for adding two numbers together """
     def test_add(self):
-        self.assertEqual(add_numbers(3, 8), 11)
+        self.assertEqual(add(3, 8), 11)
+
+    def test_substract(self):
+        self.assertEqual(substract(4, 2), 2)
