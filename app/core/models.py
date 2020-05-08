20559aebf86729d73ecfddcf2c3e929fd1109f3b
A-7222 <richard.j.ext@flydubai.com>
2020-05-08 08:36:28 +0530

    +1

--- app/core/models.py
+++ app/core/models.py
@@ -13,15 +13,16 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
+
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
         """ method to create a super user """
-        user = self.model(email=self.normalize_email(email))
-        user.set_password(password)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save()
+        user.save(using=self.db)
+
         return user
 
 
