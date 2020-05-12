000acf6447b095d4060828939e2450ecb5a42f50
A-7222 <richard.j.ext@flydubai.com>
2020-05-12 14:09:32 +0530

    +1

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -99,7 +99,7 @@ class PublicUserAPITest(TestCase):
     def test_create_token_with_missing_fields(self):
         """ test if token is not issued with missing fields """
         res = self.client.post(
-            TOKEN_URL,{'email': 'mail', 'password': 'lulz'})
+            TOKEN_URL, {'email': 'mail', 'password': 'lulz'})
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
