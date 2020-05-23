5e4bd766bdad13434ed83403fc6c2b5cfa8851a8
A-7222 <richard.j.ext@flydubai.com>
2020-05-23 11:12:19 +0530

    +1

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,6 +10,7 @@ def sample_user(email='john7ric@mail.com', password='open@123'):
     """
     return get_user_model().objects.create_user(email, password)
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_succesful(self):
